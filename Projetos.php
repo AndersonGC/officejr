73068c0a569d23bb82a6919d516789fa9be41011
Anderson <andeersongc@gmail.com>
2019-06-30 21:07:01 -0300

    segura esse commit ai pai

--- Projetos.php
+++ Projetos.php
@@ -15,17 +15,19 @@
     $qr = $conn->query($sql);
     //enquanto existir registro retornado na consulta, carrega no dropdown de cidade
     while ($linha = $qr->fetch_array()) { 
+    $dataP = explode('-', $linha['prazoDeEntrega']); 
+    $dataParaExibir = $dataP[2].'/'.$dataP[1].'/'.$dataP[0];
     ?>
 
     <div class="card border mb-3" style="max-width: 18rem;">
-        <div class="card-header"><h5><i class= "fas fa-user"></i>  <?php echo $linha['nome']?></h5></div>
+        <div class="card-header"><h5><i class= "fas fa-folder"></i>  <?php echo $linha['nome']?></h5></div>
         <div class="card-body text-secondary">
-        <p class="card-text"><i class= "fas fa-phone"></i>  <?php echo $linha['descricao']?><br>
-        <i class= "fas fa-book"></i>  <?php echo $linha['nome']?> </p>
+        <p class="card-text"><i class="fas fa-globe"></i>  <?php echo $linha['descricao']?><br>
+        <i class="fas fa-calendar-alt"></i>  <?php echo $dataParaExibir?></p>
 
         <a href= "editarProjeto.php?cpf=<?php echo $linha['codProjeto']; ?> " class=" btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
 
-        <a href= "removerProjeto.php?codProjeto=<?php echo $linha['codProjeto']; ?> " class=" btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
+        <a href= "removerProjeto.php?codProjeto=<?php echo $linha['codProjeto']; ?> " class=" btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
 
 
 
